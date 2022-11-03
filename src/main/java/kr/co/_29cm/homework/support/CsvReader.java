b6b46db636f9afcbfdc2a553d678012a70005511
hyeonbinsa <shb1833@gmail.com>
2022-11-03 14:37:21 +0900

    feat: CsvReader 기능 구현

---
+++ src/main/java/kr/co/_29cm/homework/support/CsvReader.java
@@ -0,0 +1,45 @@
+package kr.co._29cm.homework.support;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvReader {
+
+    private static final String CSV_DIVIDER = ",";
+
+    public static List<String[]> read(String fileName) {
+        final File file = findFilePath(fileName);
+        try (final FileReader fileReader = new FileReader(file);
+             final BufferedReader reader = new BufferedReader(fileReader)) {
+            return generateFileContents(reader);
+        } catch (IOException exception) {
+            throw new IllegalArgumentException("올바르지 않은 파일명입니다.");
+        }
+    }
+
+    private static File findFilePath(String fileName) {
+        final URL resource = CsvReader.class.getClassLoader().getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("올바르지 않은 파일명입니다.");
+        }
+
+        return new File(resource.getFile());
+    }
+
+    private static List<String[]> generateFileContents(BufferedReader reader) throws IOException {
+        final List<String[]> fileContents = new ArrayList<>();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            final String[] lineItems = line.split(CSV_DIVIDER);
+            fileContents.add(lineItems);
+        }
+
+        return fileContents;
+    }
+}
---
+++ src/test/java/kr/co/_29cm/homework/support/CsvReaderTest.java
@@ -0,0 +1,29 @@
+package kr.co._29cm.homework.support;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CsvReaderTest {
+
+    @DisplayName("CSV 파일을 읽어 List 형태로 반환한다.")
+    @Test
+    void read() {
+        // when
+        List<String[]> read = CsvReader.read("items.csv");
+
+        // then
+        assertEquals(read.size(), 6);
+    }
+
+    @DisplayName("파일명이 존재하지 않을 경우 예외를 반환한다.")
+    @Test
+    void read_WhenNonExistFile() {
+        // when & then
+        assertThrows(IllegalArgumentException.class,
+                () -> CsvReader.read("item.csv"));
+    }
+}
