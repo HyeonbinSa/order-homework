8188d753aebe855a0a9e76b984c550ef9bb26d86
hyeonbinsa <shb1833@gmail.com>
2022-11-03 19:58:21 +0900

    feat: 상품 번호 및 수량 입력 기능 구현

--- src/main/java/kr/co/_29cm/homework/controller/OrderController.java
+++ src/main/java/kr/co/_29cm/homework/controller/OrderController.java
@@ -1,12 +1,15 @@
 package kr.co._29cm.homework.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import kr.co._29cm.homework.domain.Product;
 import kr.co._29cm.homework.service.ProductService;
 import kr.co._29cm.homework.view.InputView;
 import kr.co._29cm.homework.view.OutputView;
 
 public class OrderController {
+    public static final String EMPTY = "";
     final ProductService productService = new ProductService();
 
     public void run() {
@@ -17,6 +20,24 @@ public class OrderController {
         productService.init();
         List<Product> products = productService.findAll();
         OutputView.printProducts(products);
+        // 주문 시작
+        Map<Long, Integer> orderRequests = generateOrderRequest();
+    }
+
+    private Map<Long, Integer> generateOrderRequest() {
+        Map<Long, Integer> orderRequests = new HashMap<>();
+        while (true) {
+            Long productId = InputView.inputProductId();
+            Integer stock = InputView.inputStock();
+            if (productId == null || stock == null) {
+                return orderRequests;
+            }
+            if (orderRequests.containsKey(productId)) {
+                orderRequests.put(productId, orderRequests.get(productId) + stock);
+                continue;
+            }
+            orderRequests.put(productId, 0);
+        }
     }
 
     private boolean inputCommand() {
--- src/main/java/kr/co/_29cm/homework/view/InputView.java
+++ src/main/java/kr/co/_29cm/homework/view/InputView.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in);
+    public static final String EMPTY = "";
 
     public static boolean inputStartCommand() {
+
         System.out.print("입력(o[order]: 주문, q[quit]: 종료) : ");
-        String command = scanner.next();
+        String command = SCANNER.nextLine();
         if (command.equals("o") || command.equals("order")) {
             return true;
         }
@@ -16,5 +18,33 @@ public class InputView {
             return false;
         }
         throw new IllegalArgumentException("명령어는 o[order], q[quit]만 사용할 수 있습니다.");
+    }
+
+    public static Long inputProductId() {
+        System.out.print("상품 번호 : ");
+        String input = SCANNER.nextLine();
+        if (input.equals(EMPTY)) {
+            return null;
+        }
+        try {
+            return Long.parseLong(input);
+        } catch (NumberFormatException e) {
+            System.out.println("상품 번호는 숫자만 입력 가능합니다.");
+            return inputProductId();
+        }
+    }
+
+    public static Integer inputStock() {
+        System.out.print("수량 : ");
+        String input = SCANNER.nextLine();
+        if (input.equals(EMPTY)) {
+            return null;
+        }
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("수량은 숫자만 입력 가능합니다.");
+            return inputStock();
+        }
     }
 }
